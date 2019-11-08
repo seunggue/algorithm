ced67c1c6b5a5d0776d7e11c37ac1eb35a56f4b2
seunggue <seungue1687@gmail.com>
2019-11-08 17:56:11 +0900

    test

--- ssafy.py
+++ ssafy.py
@@ -1,49 +1,27 @@
 import sys
 sys.stdin = open('input.txt','r')
 
-def num_make(opt_idx, idx, num, opt_list):
-    global n, board, opt, max_num, min_num
-    print(opt_list)
-    print(opt_idx)
-    opt_list[opt_idx] += 1
-
-    if opt_list[opt_idx] > opt[opt_idx]:
-        return
-
-    if opt_idx == 0:
-        num += board[idx+1]
-    elif opt_idx == 1:
-        num -= board[idx+1]
-    elif opt_idx == 2:
-        num *= board[idx+1]
-    else:
-        num = int(num/board[idx+1])
-
-    if idx + 1 == n:
-        if num > max_num:
-            max_num = num
-        if num < min_num:
-            min_num = num
-
-
-    for i in range(4):
-        num_make(i,idx+1,num, opt_list[i]+1)
-
-
 
 for t_c in range(int(input())):
     n = int(input())
-    opt = list(map(int, input().split()))
-    board = list(map(int, input().split()))
-    opt_list = [0,0,0,0]
-    max_num = 0
-    min_num = 100000000
 
-    for i in range(4):
-        num_make(i,0, board[0], opt_list)
+    food = [list(map(int, input().split())) for _ in range(n)]
+    minS = 1000000
+    for a_i in range(n):
+        for a_j in range(a_i+1,n):
+            check = []
+            a = food[a_i][a_j] + food[a_j][a_i]
+            check.append(a_i)
+            check.append(a_j)
+            for b_i in range(n):
+                for b_j in range(b_i+1,n):
 
-
-    print(max_num-min_num)
-
-
+                    if b_i not in check and b_j not in check:
+                        b = food[b_i][b_j] + food[b_j][b_i]
+                        print(check)
+                        print(b_i, b_j)
+                        print(a, b)
+                        if abs(a-b) < minS:
+                            minS = abs(a-b)
+    print(minS)
 
--- test.py
+++ test.py
@@ -1,2 +1,3 @@
-for i in range(4):
-    print(type(i))
+a= []
+a.append(3)
+print(a)
